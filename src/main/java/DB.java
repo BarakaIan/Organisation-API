478dc0c908e486b50f40906a0859011aef450527
BarakaIan <ianb.makale@gmail.com>
2021-03-08 16:46:31 +0300

    add changes to the DB.java file

--- src/main/java/DB.java
+++ src/main/java/DB.java
@@ -2,5 +2,5 @@ import org.sql2o.Sql2o;
 
 public class DB {
 
-    /rganisational", "moringa", "D2000sep");
+    public static Sql2o sql2o = new Sql2o("jdbc:postgresql://localhost:5432/organisational", "moringa", "D2000sep");
 }
